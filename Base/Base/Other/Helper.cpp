0a6a177e2d23690746d27387b43c049894608061
Sailordi <sailordi11@gmail.com>
2021-04-20 02:05:32 +0300

    Added a function that exits the program from anywhere in the program

--- Base/Base/Other/Helper.cpp
+++ Base/Base/Other/Helper.cpp
@@ -1,2 +1,11 @@
 #include "Helper.h"
 
+#include <cstdlib>
+
+#include <QDebug>
+
+//Public functions
+void Helper::quitProgram(QString msg,int code) {
+    qDebug().noquote()<<msg;
+    exit(code);
+}
--- Base/Base/Other/Helper.h
+++ Base/Base/Other/Helper.h
@@ -1,8 +1,12 @@
 #ifndef HELPER_H
 #define HELPER_H
 
+#include <QString>
+
 /*!
  * \brief The namespace contains help functions
+ *
+ * Incudes: QString
  *
  * \author Created by: Sailordi
  * \author Last to touch it: Sailordi
@@ -11,6 +15,11 @@
  * \date Last update: 2021-04-20
 */
 namespace Helper {
+    /*! Exits the program from the current place
+     * \param msg - Message to be shown
+     * \param code - The exit code
+    */
+    void quitProgram [[noreturn]](QString msg,int code);
 
 };
 #endif // HELPER_H
