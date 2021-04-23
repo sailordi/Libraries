dddc7853e4ae8e4cc557c5a28aae94a6820fffa9
Sailordi <sailordi11@gmail.com>
2021-04-23 15:45:27 +0300

    Added a function that calculates how many threads should be used

--- Base/Base/Thread/ThreadCalculator.cpp
+++ Base/Base/Thread/ThreadCalculator.cpp
@@ -1,2 +1,32 @@
 #include "ThreadCalculator.h"
 
+#include <QtMath>
+
+//Public functions
+int ThreadCalculator::calculateThreads(int size,int& workload,int maxThreads,int& to,int& toModifier) {
+    int t = maxThreads;
+
+        if(size <= 0) {
+            to = 0;
+            toModifier = 0;
+            return 0;
+        }
+
+        int workPerThread = ceil(size/t);
+        int threadsForWork = ceil(size/workload);
+
+        if(size < workload) {
+            t = 1;
+        }
+        else if(t < threadsForWork) {
+            workload = workPerThread;
+        }
+        else {
+            t = threadsForWork;
+        }
+
+        to = ceil(size/t);
+        toModifier = to;
+
+        return t;
+}
--- Base/Base/Thread/ThreadCalculator.h
+++ Base/Base/Thread/ThreadCalculator.h
@@ -11,6 +11,15 @@
  * \date Last update: 2021-04-23
 */
 namespace ThreadCalculator {
+    /*! Calculates how many threads should be used
+     * \param size - The data size
+     * \param workload - The workload per thread
+     * \param maxThreads - Max number of threads to be used
+     * \param to - The position to loop to
+     * \param toModifier - The amount to should be modified every time
+     * \returns int
+    */
+    int calculateThreads(int size,int& workload,int maxThreads,int& to,int& toModifier);
 
 };
 #endif // THREADCALCULATOR_H
